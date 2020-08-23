fb44954236e9e20cee8d94cb597bfbcb5f703947
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-08-23 22:43:43 +0700

    init file

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = 'https://jsonplaceholder.typicode.com/todos/1';
+axios_1["default"].get(url).then(function (response) { return console.log(response.data); });
